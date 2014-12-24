23cb91381f924e6846d967cf788220edcc74c2ee
Roy van der Meij <roy@royapps.nl>
2014-12-24 19:36:14 +0100

    correct version for writing to spidev

--- lib/ledstrip/type/lpd6803.rb
+++ lib/ledstrip/type/lpd6803.rb
@@ -1,47 +1,55 @@
 module Ledstrip
   module Type
     class LPD6803
-      def initialize
+      def initialize(order: [:r, :g, :b])
+        @order = order
       end
 
       def draw(leds)
-        write_start_bytes
-        write_leds(leds)
-        write_end_bytes(leds.size)
+        bytes = []
+        bytes << start_bytes
+        bytes << led_bytes(leds)
+        bytes << end_bytes(leds.size)
+
+        write(bytes.flatten)
       end
 
       def write(array)
-        #PiPiper::Spi.spidev_out(array)
         File.open('/dev/spidev0.0', 'wb'){|f| f.write(array.pack('C*')) }
       end
 
       private
-      def write_start_bytes
+      def start_bytes
         # 32 bits start frame
-        write([0,0,0,0])
+        [0,0,0,0]
       end
 
-      def write_leds(leds)
-        leds.each do |led|
-          write_led(led)
-        end
+      def led_bytes(leds)
+        bytes = leds.collect do |led|
+          led_byte(led)
+        end.flatten
       end
 
-      def write_led(led)
+      def led_byte(led)
         bits = 0b1000000000000000
-        bits |= ((led.r / 255) * 31) << 10
-        bits |= ((led.g / 255) * 31) << 5
-        bits |= ((led.b / 255) * 31) 
+        bits |= ((first_part(led)  / 255) * 31).to_i << 10
+        bits |= ((second_part(led) / 255) * 31).to_i << 5
+        bits |= ((third_part(led)  / 255) * 31).to_i 
 
         output = []
         output[0] = (bits & 0xff00) >> 8
         output[1] = (bits & 0x00ff) >> 0
-        write(output)
+
+        output
       end
 
-      def write_end_bytes(size)
+      def first_part(led);  led.send(@order[0]).to_f; end
+      def second_part(led); led.send(@order[1]).to_f; end
+      def third_part(led);  led.send(@order[2]).to_f; end
+
+      def end_bytes(size)
         # two versions found online, one is to use bits per led other is bytes per led
-        write(Array.new(size / 8 + 1, 0))
+        Array.new(size / 8 + 1, 0)
         # write(Array.new(size, 0))
       end
     end
--- spec/ledstrip/type/lpd6803_spec.rb
+++ spec/ledstrip/type/lpd6803_spec.rb
@@ -7,10 +7,29 @@ describe LPD6803 do
 
   describe "#draw" do
 
-    xit "draws a red led" do
+    it "draws a red led" do
       subject = LPD6803.new
-      red_led = Led.new(r: 255, g: 0, b: 0)
+      red_led = Ledstrip::Led.new(r: 255, g: 0, b: 0)
+
+      allow(subject).to receive(:write)
       subject.draw([red_led])
+
+      result = [0,0,0,0,0b11111100, 0b00000000, 0]
+      expect(subject).to have_received(:write).with(result)
+    end
+
+    context "with different order" do
+      it "draws a red led" do
+        subject = LPD6803.new(order: [:g, :r, :b])
+        red_led = Ledstrip::Led.new(r: 255, g: 0, b: 0)
+
+        allow(subject).to receive(:write)
+
+        result = [0,0,0,0,0b10000011, 0b11100000, 0]
+        subject.draw([red_led])
+
+        expect(subject).to have_received(:write).with(result)
+      end
     end
 
   end
