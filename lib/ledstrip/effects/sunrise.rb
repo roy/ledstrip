3f879d133f367ee8e1ef81439929073740da5e4b
Roy van der Meij <roy@royapps.nl>
2014-12-29 21:43:11 +0100

    sunrise fixes

--- lib/ledstrip/effects/sunrise.rb
+++ lib/ledstrip/effects/sunrise.rb
@@ -4,16 +4,14 @@ module Ledstrip
 
       def initialize(strip, options = {})
         super
-        @start = Time.now
-        @duration = options.fetch(:duration, 30)
-        @finish = @start + @duration
-        @sequence = generate_sequence
-        @end_led = Ledstrip::Led.new(r: 255, g: 255, b: 255)
+        @start = Time.now.to_i
+        @duration = options.fetch("duration", 30)
+        @finish = @start + @duration.to_i
       end
 
       def position
-        time = Time.now
-        pos = time > @finish ? 1 : (time - @start)/@duration
+        time = Time.now.to_i
+        pos = time > @finish ? 1 : (time - @start).to_f/@duration
       end
 
       def step(leds)
@@ -41,38 +39,9 @@ module Ledstrip
       end
 
       def sleep_time
-        2
+        0.1
       end
 
-      private
-      def generate_sequence
-        sequence_count = @duration * (1 / sleep_time)
-        #sequence = Array.new(sequence_count, Ledstrip::Led.new(r: 0, g: 0, b: 0))
-        sequence = []
-
-        max_red = 255
-        
-        half = (sequence_count / 2).to_i
-        other_half = (sequence_count - half).to_i
-
-        half.times do |x|
-          led = Led.new(r: 0, g: 0, b: 0)
-          led.red = (x.to_f / half) * max_red
-
-          sequence << led
-        end
-
-        other_half.times do |x|
-          led = Led.new(r: 0, g: 0, b: 0)
-          led.red = 255 
-          led.green = (x.to_f / other_half) * 255
-          led.blue = (x.to_f / other_half) * 255
-
-          sequence << led
-        end
-
-        sequence
-      end
     end
   end
 end
